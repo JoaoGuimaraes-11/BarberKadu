9e161a9b4d2876243d88ace6beb4d8cf6be9e365
JoaoGuimaraes-11 <jonyjocas11@gmail.com>
2025-04-15 21:00:02 -0300

    alteracoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@ const btnChange = document.querySelector('#openMenu');
 function mobileNav(){
     btnChange.addEventListener('click', ()=>{
         const contentMobile = document.querySelector('.nav-mobile');
-        const hamburguerModal = document.querySelector('.fa-bars')
+        const hamburguerModal = document.querySelector('.fa-bars');
+        const openItens = document.querySelector('.itens-mobile');
         hamburguerModal.classList.toggle('fa-x')
+        openItens.classList.toggle('active')
         contentMobile.classList.toggle('active')
     })
 }
